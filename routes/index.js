4778eb30ed5e072d3f2cec7d1cbc89c8f70f7912
Maurivan Braz <maurivan.braz@gmail.com>
2020-06-22 14:38:23 -0300

    Segundo commit teste

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Maurivan' });
+  res.render('index', { title: 'Maurivan Teste ' });
 });
 
 module.exports = router;
